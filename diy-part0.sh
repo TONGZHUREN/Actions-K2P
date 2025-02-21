11c5ed216dfc55dfc7c94190d622fdf42828621f
TONGZHUREN <32959720+TONGZHUREN@users.noreply.github.com>
2025-02-21 17:59:01 +0800

    Update diy-part0.sh

--- diy-part0.sh
+++ diy-part0.sh
@@ -19,6 +19,7 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 
 # Define My Package
@@ -34,3 +35,4 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
