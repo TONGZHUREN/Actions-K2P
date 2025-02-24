86a5828bf804d1d73c3ecd528c02db3b951d8b3c
TONGZHUREN <32959720+TONGZHUREN@users.noreply.github.com>
2025-02-25 05:54:43 +0800

    Update diy-part0.sh

--- diy-part0.sh
+++ diy-part0.sh
@@ -24,7 +24,7 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # Define My Package
 
-#git clone https://github.com/thinktip/luci-theme-neobird package/molun/luci-theme-neobird
+git clone https://github.com/thinktip/luci-theme-neobird package/molun/luci-theme-neobird
 #git clone https://github.com/tracemouse/luci-theme-netgear package/molun/luci-theme-netgear
 #git clone https://github.com/kenzok8/golang feeds/packages/lang/golang 
 
